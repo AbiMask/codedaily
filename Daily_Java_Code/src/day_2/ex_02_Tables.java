2a5f08dcec3c8fa08a0eee35a5c0b015e55aff26
AbiMask <abisubimask@gmail.com>
2025-06-16 00:01:28 +0530

    16/06

---
+++ Daily_Java_Code/src/day_2/ex_02_Tables.java
@@ -0,0 +1,12 @@
+package day_2;
+
+import java.util.Iterator;
+
+public class ex_02_Tables {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <=10 ; i++) {
+			System.out.printf("%d x %d = %d\n",n,i,n*i);
+		}
+	}
+}
