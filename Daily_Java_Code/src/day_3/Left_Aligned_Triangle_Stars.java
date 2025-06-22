a21ef29d113c3d5c677c9e4acb8c1ad356bce210
AbiMask <abisubimask@gmail.com>
2025-06-22 16:51:30 +0530

    22/06/2025

---
+++ Daily_Java_Code/src/day_3/Left_Aligned_Triangle_Stars.java
@@ -0,0 +1,22 @@
+package day_3;
+
+public class Left_Aligned_Triangle_Stars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int n = 5;
+		
+		
+		for(int i = 1;  i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
