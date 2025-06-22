8dddb041d2257ed180e39b21ac05fcb9c4ef8723
AbiMask <abisubimask@gmail.com>
2025-06-22 16:51:38 +0530

    22/06/2025

---
+++ Daily_Java_Code/src/day_3/Pyramid_of_Stars.java
@@ -0,0 +1,26 @@
+package day_3;
+
+import java.util.Iterator;
+
+public class Pyramid_of_Stars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		
+		for (int i = 1; i <= n; i++) {
+		    // Print leading spaces (2 spaces each)
+		    for (int j = i; j < n; j++) {
+		        System.out.print(" ");
+		    }
+		    
+		    // Print stars with a space after each
+		    for (int k = 1; k <= 2*i-1; k++) {
+		        System.out.print("*");
+		    }
+		    
+		    System.out.println();
+		}
+	}
+
+}
